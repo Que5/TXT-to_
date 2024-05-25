8a6a9f4edcbcce7a25f66b20e12fc83ca3008d39
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-25 09:49:23 +0200

    Added data onto the pdf

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ pdf = FPDF(orientation="P", unit="mm", format="A4",)
 
 for filepath in filepaths:
     with open(f'{filepath}') as file:
-        data = file.readlines()
+        data = file.read()
         pdf.add_page()
         filename = Path(filepath).stem
         name = filename.capitalize()
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"{name}")
+        pdf.cell(w=50, h=8, txt=f"{name}", ln=1)
+    
+        
+        pdf.set_font(family="Times", size=10)        
+        pdf.multi_cell(w=0, h=6, txt=str(data))
 
 
-pdf.output(f"PDFs/{filename}.pdf")
+pdf.output(f"PDFs/output.pdf")
        
         
