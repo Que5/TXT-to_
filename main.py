0b4f68ee181a4ce6535a76fa024f0b1579f9b159
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-25 08:06:34 +0200

    Made some changes, removed the other pdfs

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from pathlib import Path
 
 filepaths = glob.glob("txtfiles/*txt")
 
+pdf = FPDF(orientation="P", unit="mm", format="A4",)
+
 for filepath in filepaths:
     with open(f'{filepath}') as file:
         data = file.readlines()
-        pdf = FPDF(orientation="P", unit="mm", format="A4",)
-        get_title = data[0]
         pdf.add_page()
         filename = Path(filepath).stem
+        name = filename.capitalize()
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"{get_title}")
-        pdf.output(f"PDFs/{filename}.pdf")
+        pdf.cell(w=50, h=8, txt=f"{name}")
+
+
+pdf.output(f"PDFs/{filename}.pdf")
        
         
